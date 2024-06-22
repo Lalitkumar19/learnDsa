b6d20eaaf94a809d36ead013b5294ce2371f66e9
Lalit kumar <klalit0859@gmail.com>
2024-06-22 14:02:58 +0530

    Add files via upload
    Write a Java program that calculates and print the sum of two integers 'a' and 'b'.

---
+++ DataTypes.java
@@ -0,0 +1,13 @@
+
+public class DataTypes
+{
+    public static void main(String[] args) {
+        
+        int a  = 10 ;
+        int b  = 20 ;
+        // write your code logic and print the result ..
+        int sum = a + b;
+        // Print the result
+        System.out.println(sum);
+    }
+}
