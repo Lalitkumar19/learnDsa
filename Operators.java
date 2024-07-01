71ec47e3bdf39356b1aa9202770595c0fe968509
Lalit kumar <klalit0859@gmail.com>
2024-07-01 11:44:09 +0530

    Arithmatic, Assignment, Augmented, Precedence

---
+++ Operators.java
@@ -0,0 +1,8 @@
+class Operators {
+    public static void main(String[] args) {
+        //Augmented operators (+=,-=,*=./=)
+        int num = 50;
+        num += 50;
+        System.out.println(num);
+    }
+}
